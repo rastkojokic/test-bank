bc198224d598ccd12016e932a8c35903730a1fe7
Rastko Jokic <rastko@renderedtext.com>
2015-09-19 08:17:19 +0000

    Init testing dependencies

--- Gemfile
+++ Gemfile
@@ -14,3 +14,13 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   gem 'byebug'
 end
+
+group :test do
+  gem 'factory_girl'
+  gem 'factory_girl_rails'
+  gem 'rspec-activemodel-mocks'
+  gem 'rspec-its'
+  gem 'rspec-rails'
+  gem 'shoulda'
+  gem 'database_cleaner'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+RSpec.configure do |config|
+  ENV["RAILS_ENV"] ||= 'test'
+
+  require File.expand_path("../../config/environment", __FILE__)
+
+  config.include FactoryGirl::Syntax::Methods
+  
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
